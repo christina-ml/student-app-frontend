9eab95ead76241bc30ac5a530b91a0b7340cc873
Christina Loiacono <loiaconochristina@gmail.com>
2022-07-15 13:33:10 -0400

    started navbar for desktop

--- src/components/studentCard/StudentCard.js
+++ src/components/studentCard/StudentCard.js
@@ -1,10 +1,12 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link } from "react-router-dom";
 
 import SingleTextInput from '../singleTextInput/SingleTextInput';
+import EmptyView from '../emptyView/EmptyView';
 
 import { FaPlus, FaMinus } from 'react-icons/fa';
-import {AiOutlineLoading} from 'react-icons/ai';
+import {AiOutlineReload } from 'react-icons/ai';
+
 
 import './StudentCard.scss';
 
@@ -19,7 +21,6 @@ const StudentCard = ({student}) => {
     const [gradesLoading, setGradesLoading] = useState(false);
     const [tags, setTags] = useState([]);
     const [tag, setTag] = useState('');
-
     console.log("tag:", tag);
 
     // functions 
@@ -60,8 +61,13 @@ const StudentCard = ({student}) => {
             fetch(url)
                 .then(response => response.json())
                 .then(data => {
-                    setGrades(data);
-                    setGradesLoading(false); // after the grades have loaded
+                    if (data.length){
+                        setGrades(data);
+                        setGradesLoading(false); // after the grades have loaded
+
+                    } else {
+
+                    }
                 })
             }
         }
@@ -76,57 +82,59 @@ const StudentCard = ({student}) => {
     return (
         <div className="studentCard">
             <Link to={`/students/${student.id}`} state={{ student: student }}>
-            <div className="studentCard__profilePic">
-                <img src={pic} />
-            </div>
             
-            <div className="studentCard__info">
-                <div className="studentCard__name">
-                    {`${firstname}  ${lastname}`}
+                <div className="studentCard__profilePic">
+                    <img src={pic} />
                 </div>
-                <div className="studentCard__infoLine">
-                    Email: {email}
-                </div>
-                <div className="studentCard__infoLine">
-                    Company: {company}
-                </div>
-                <div className="studentCard__infoLine">
-                    Skill: {skill}
-                </div>
-                
-                <div className="studentCard__gradesList" style={{"display": showGrades ? "block" : "none"}}>
-                    <div className="studentCard__gradeAverage">
-                        Average: {grades.length && calculateAverage(grades)}%
+            
+                <div className="studentCard__info">
+                    <div className="studentCard__name">
+                        {`${firstname}  ${lastname}`}
                     </div>
-                    {grades.map((grade, index) => {
-                        return (
-                            <div key={index}><span>Test {index+1}:</span><span>{grade.grade}%</span></div>
-                        )
-                    })}
-                </div>
-            </div>
-            
-            <div className="studentCard__toggleIcons">
-                {/* modifier spinning */}
-                {gradesLoading && <AiOutlineLoading className="studentCard__toggleIcon-spinning" size="1.8em" />}
-                {(!showGrades && !gradesLoading) && <FaPlus className="studentCard__toggleIcon" onClick={(e) => fetchAndShowGrades(e)} size="1.8em"/>}
-                {(showGrades && !gradesLoading) && <FaMinus className="studentCard__toggleIcon" onClick={(e) => hideGrades(e)} size="1.8em" />}
-            </div>
-            </Link>
+                    <div className="studentCard__infoLine">
+                        Email: {email}
+                    </div>
+                    <div className="studentCard__infoLine">
+                        Company: {company}
+                    </div>
+                    <div className="studentCard__infoLine">
+                        Skill: {skill}
+                    </div>
 
+                    <div className="studentCard__gradesList" style={{"display": showGrades ? "block" : "none"}}>
+                        {grades.length > 0 && 
+                        <>
+                            <div className="studentCard__gradeAverage">
+                                Average: {grades.length && calculateAverage(grades)}%
+                            </div>
+                            {grades.map((grade, index) => {
+                                return (
+                                    <div key={index}><span>Test {index+1}:</span><span>{grade.grade}%</span></div>
+                                )
+                            })}
+                        </>
+                        }
+                        {grades.length === 0 && <EmptyView />}
+                    </div>
+                </div>
+                <div className="studentCard__toggleIcons">
+                    {gradesLoading && <AiOutlineReload className="studentCard__toggleIcon-spinning" size="1.8em" />}
+                    {(!showGrades && !gradesLoading) && <FaPlus className="studentCard__toggleIcon" onClick={(e) => fetchAndShowGrades(e)} size="1.8em"/>}
+                    {(showGrades && !gradesLoading) && <FaMinus className="studentCard__toggleIcon" onClick={(e) => hideGrades(e)} size="1.8em" />}
+                 </div>
+            </Link> 
             <div className="studentCard__tagCollection">
-                    <div className="studentCard__tags">
-                        {tags.map((tag, index) => {
-                            return (
-                                <span className="studentCard__tag" key={tag + index} >{tag}</span>
-                            )
-                        })}
+                        <div className="studentCard__tags">
+                            {tags.map((tag, index) => {
+                                return (
+                                    <span className="studentCard__tag" key={tag + index}>{tag}</span>
+                                )
+                            })}
+                        </div>
+                        <div className="studentCard__tagInput">
+                            <SingleTextInput onSubmit={setTags} collection={tags} searchTerm={tag} setSearchTerm={setTag} width="26%" placeholder="Add a tag" />
+                        </div>
                     </div>
-                    <div className="studentCard__tagInput">
-                        <SingleTextInput onSubmit={setTags} collection={tags} searchTerm={tag} setSearchTerm={setTag} width="26%" placeholder="Add a tag" />
-                    </div>
-                </div>
-
         </div>
     )
 }
