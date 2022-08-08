ad81a58fb4ff54b74db6909f19bd5cc0df1bd0c5
Christina Loiacono <loiaconochristina@gmail.com>
2022-08-08 13:25:54 -0400

    fix add tag field for student card

--- src/components/studentCard/StudentCard.js
+++ src/components/studentCard/StudentCard.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import { Link, useNavigate } from "react-router-dom";
 
-import SingleTextInput from '../singleTextInput/SingleTextInput';
+import TagInputField from '../tagInputField/TagInputField';
 import EmptyView from '../emptyView/EmptyView';
 import DialogBox from '../dialogBox/DialogBox';
 
@@ -25,7 +25,6 @@ const StudentCard = ({student, showDelete=false}) => {
     const [showGrades, setShowGrades] = useState(false);
     const [gradesLoading, setGradesLoading] = useState(false);
     const [tags, setTags] = useState([]);
-    const [tag, setTag] = useState('');
     const [showDeleteDialog, setShowDeleteDialog] = useState(false);
     const [deleteUserLoading, setDeleteUserLoading] = useState(false);
     const [showSnackbar, setShowSnackbar] = useState(false);
@@ -89,103 +88,103 @@ const StudentCard = ({student, showDelete=false}) => {
 
     
         fetch(url, { method: 'DELETE' })
-            .then(response =>  response.json())
-            .then(data => {
-                // redirect to home page
-                navigate("/", { 
-                    state: {
-                        studentName: `${data.firstname} ${data.lastname}`
-                    }
-                });
+        .then(response =>  response.json())
+        .then(data => {
+            // redirect to home page
+            navigate("/", { 
+                state: {
+                    studentName: `${data.firstname} ${data.lastname}`
+                }
+            });
 
-                setDeleteUserLoading(false)
-            }).catch(err => {
-                // show toast that delete was unsuccessful
-                setDeleteUserLoading(false)
-                setShowSnackbar(true);
-            })
+            setDeleteUserLoading(false)
+        }).catch(err => {
+            // show toast that delete was unsuccessful
+            setDeleteUserLoading(false)
+            setShowSnackbar(true);
+        })
 
-    }
+}
 
-    // useEffect(() => {
-    //     if(grades.length)
-    //         setShowGrades(!showGrades);
-    // }, [grades])
+// useEffect(() => {
+//     if(grades.length)
+//         setShowGrades(!showGrades);
+// }, [grades])
 
-    return (
-        <div className="studentCard">
-            <Snackbar 
-                open={showSnackbar} 
-                anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-                autoHideDuration={1500}
-                onClose={() => setShowSnackbar(false)}>
-                <Alert severity="error">An error occurred while deleting — try again later.</Alert>
-            </Snackbar>
+return (
+    <div className="studentCard">
+        <Snackbar 
+            open={showSnackbar} 
+            anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+            autoHideDuration={1500}
+            onClose={() => setShowSnackbar(false)}>
+            <Alert severity="error">An error occurred while deleting — try again later.</Alert>
+        </Snackbar>
 
-            <Link to={`/students/${id}`} state={{ student: student }}>
-            
-                <div className="studentCard__profilePic">
-                    <img src={pic} />
+        <Link to={`/students/${id}`} state={{ student: student }}>
+        
+            <div className="studentCard__profilePic">
+                <img src={pic} />
+            </div>
+        
+            <div className="studentCard__info">
+                <div className="studentCard__name">
+                    {`${firstname}  ${lastname}`}
                 </div>
-            
-                <div className="studentCard__info">
-                    <div className="studentCard__name">
-                        {`${firstname}  ${lastname}`}
-                    </div>
-                    <div className="studentCard__infoLine">
-                        Email: {email}
-                    </div>
-                    <div className="studentCard__infoLine">
-                        Company: {company}
-                    </div>
-                    <div className="studentCard__infoLine">
-                        Skill: {skill}
-                    </div>
+                <div className="studentCard__infoLine">
+                    Email: {email}
+                </div>
+                <div className="studentCard__infoLine">
+                    Company: {company}
+                </div>
+                <div className="studentCard__infoLine">
+                    Skill: {skill}
+                </div>
 
-                    <div className="studentCard__gradesList" style={{"display": showGrades ? "block" : "none"}}>
-                        {grades.length > 0 &&
-                        <>
-                            <div className="studentCard__gradeAverage">
-                                Average: {grades.length && calculateAverage(grades)}%
-                            </div>
-                            {grades.map((grade, index) => {
-                                return (
-                                    <div key={index}><span>Test {index+1}:</span><span>{grade.grade}%</span></div>
-                                )
-                            })}
-                        </>
-                        }
-                        {grades.length === 0  && <EmptyView text="No Grades for this Student"/>}
-                    </div>
-                </div>
-                <div className="studentCard__actionIcons">
-                    {gradesLoading && <AiOutlineReload className="studentCard__toggleIcon-spinning" size="1.8em" />}
-                    {(!showGrades && !gradesLoading) && <FaPlus className="studentCard__toggleIcon" onClick={(e) => fetchAndShowGrades(e)} size="1.8em"/>}
-                    {(showGrades && !gradesLoading) && <FaMinus className="studentCard__toggleIcon" onClick={(e) => hideGrades(e)} size="1.8em" />}
-                </div>
-    
-            </Link> 
-            <div className="studentCard__tagCollectionRow">
-                <div className="studentCard__tagCollection">
-                    <div className="studentCard__tags">
-                        {tags.map((tag, index) => {
+                <div className="studentCard__gradesList" style={{"display": showGrades ? "block" : "none"}}>
+                    {grades.length > 0 &&
+                    <>
+                        <div className="studentCard__gradeAverage">
+                            Average: {grades.length && calculateAverage(grades)}%
+                        </div>
+                        {grades.map((grade, index) => {
                             return (
-                                <span className="studentCard__tag" key={tag + index}>{tag}</span>
+                                <div key={index}><span>Test {index+1}:</span><span>{grade.grade}%</span></div>
                             )
                         })}
-                    </div>
-                    <div className="studentCard__tagInput">
-                        <SingleTextInput onSubmit={setTags} collection={tags} searchTerm={tag} setSearchTerm={setTag} width="26%" placeholder="Add a tag" />
-                    </div>
+                    </>
+                    }
+                    {grades.length === 0  && <EmptyView text="No Grades for this Student"/>}
                 </div>
-                {showDelete && <div>
-                    {deleteUserLoading && <AiOutlineReload className="studentCard__toggleIcon-spinning" size="1.8em" />}
-                    {!deleteUserLoading && <FaTrash className="studentCard__trashIcon" onClick={(e) => showDeleteUserDialogue(e)} size="1.8em"/>}
-                </div> }
             </div>
-            <DialogBox open={showDeleteDialog} setOpen={setShowDeleteDialog} deleteUser={deleteUser} />
+            <div className="studentCard__actionIcons">
+                {gradesLoading && <AiOutlineReload className="studentCard__toggleIcon-spinning" size="1.8em" />}
+                {(!showGrades && !gradesLoading) && <FaPlus className="studentCard__toggleIcon" onClick={(e) => fetchAndShowGrades(e)} size="1.8em"/>}
+                {(showGrades && !gradesLoading) && <FaMinus className="studentCard__toggleIcon" onClick={(e) => hideGrades(e)} size="1.8em" />}
+            </div>
+
+        </Link> 
+        <div className="studentCard__tagCollectionRow">
+            <div className="studentCard__tagCollection">
+                <div className="studentCard__tags">
+                    {tags.map((tag, index) => {
+                        return (
+                            <span className="studentCard__tag" key={tag + index}>{tag}</span>
+                        )
+                    })}
+                </div>
+                <div className="studentCard__tagInput">
+                    <TagInputField setTags={setTags} tags={tags} id={student.id} />
+                </div>
+            </div>
+            {showDelete && <div>
+                {deleteUserLoading && <AiOutlineReload className="studentCard__toggleIcon-spinning" size="1.8em" />}
+                {!deleteUserLoading && <FaTrash className="studentCard__trashIcon" onClick={(e) => showDeleteUserDialogue(e)} size="1.8em"/>}
+            </div> }
         </div>
-    )
+        <DialogBox open={showDeleteDialog} setOpen={setShowDeleteDialog} deleteUser={deleteUser} />
+    </div>
+)
 }
 
 export default StudentCard;
