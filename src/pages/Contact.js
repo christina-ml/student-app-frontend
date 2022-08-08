affcbc7d1f7474dd99a4640ab0b17716105d9bbb
Christina Loiacono <loiaconochristina@gmail.com>
2022-08-08 12:32:18 -0400

    completed contact page, including responsive design

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -40,11 +40,24 @@ function Contact(props) {
             </div>
 
            </div>
-           <div className="contact__form">
-            Contact Form
+           <div className="contact__info">
+                <div className="contact__title">Get In Touch</div>
+                {/* optional: turn phone & email into links to call/email from site */}
+                <div className="contact__subheader"><span>Phone: </span>583-284-9987</div>
+
+                <div className="contact__subheader">Address:</div>
+                <div className="contact__subheader">123 Sesame St.<br/>Podunk, FL 49823</div>
+
+                <div className="contact__subheader"><span>Email: </span>writetome@gmail.com</div>
+
            </div>
         </div>
     );
 }
 
 export default Contact;
+
+
+// Inspiration:
+// https://www.zendesk.com/blog/contact-us-page/
+// https://www.notion.so/contact-sales
